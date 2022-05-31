ba18341f65fc2e174d8d4e895c0a72399e8e7302
SergeyVorotiy <95651513+SergeyVorotiy@users.noreply.github.com>
2022-06-01 02:55:20 +0300

    Added comments in code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-_player = 1
-def field_output(field):
+
+def field_output(field): #форматирование вывода игрового поля
     output = "  0 1 2\n"
     k = 0
     for row in field:
@@ -9,11 +9,11 @@ def field_output(field):
         output = output + "\n"
         k = k + 1
     return output
-def rec_move_in_field(x, y, char):
+def rec_move_in_field(x, y, char): #Запись хода игрока в ячейку поля
     tic_tac_toe_field[x][y] = char
 
 
-def is_game_over(field):
+def is_game_over(field): # Проверка на конец игры
     if ((field[0][0] == 'x' and field[0][1] == 'x' and field[0][2] == 'x') or (
             field[0][0] == 'o' and field[0][1] == 'o' and field[0][2] == 'o')) or (
             (field[1][0] == 'x' and field[1][1] == 'x' and field[1][2] == 'x') or (
@@ -36,21 +36,27 @@ def is_game_over(field):
             return False
         return True
 print("Поиграем в крестики нолики!")
-play_again = "y"
+#Вводим имена игроков
 player_1_name = input("Введите имя первого игрока - ")
 player_2_name = input("Введите имя второго игрока - ")
+#Цикл с проверкой на желание повторить игру
+play_again = "y"
 while play_again == "y":
     print("Начнем же игру!")
+    #формируем игровое поле и выводим его
     tic_tac_toe_field = [['-' for row in range(3)] for column in range(3)]
     print(field_output(tic_tac_toe_field))
+    #Определяем очередность ходов
     _player = 1
+    # Запускае цикл с проверкой на конец игры
     while is_game_over(tic_tac_toe_field):
+        # ход первого игрока
         if _player == 1:
             print(player_1_name)
-            x = int(input("Введите номер строки вашего хода - ") or 5)
+            x = int(input("Введите номер строки вашего хода - ") or 5)#Проверяем не вышел ли игрок за поле
             y = int(input("Введите номер столбца вашего хода - ") or 5)
             if -1<x<3 and -1<y<3:
-                if tic_tac_toe_field[x][y] == "-":
+                if tic_tac_toe_field[x][y] == "-":#Проверяем не мухлюет ли игрок
                     rec_move_in_field(x, y, "x")
                     _player = 2
                 else:
@@ -60,7 +66,7 @@ while play_again == "y":
                 print("Выберите другие координаты")
                 continue
             print(field_output(tic_tac_toe_field))
-
+        # ход второго игрока
         elif _player == 2:
             print(player_2_name)
             x = int(input("Введите номер строки вашего хода - ") or 5)
@@ -78,5 +84,7 @@ while play_again == "y":
             rec_move_in_field(x, y, "o")
 
             print(field_output(tic_tac_toe_field))
+        # Выводим сообщение о конце игры
         print("Игра окончена!\n")
+        #Спрашиваем не хотят ли игроки сыграть еше
     play_again = input("Сыграть ещё?(y/no)")
